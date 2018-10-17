1d552d76f481603f3277bbea3df6b6d99464cf7a
Robin Kim <rkim1214@gmail.com>
2018-10-17 03:00:55 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
-
+  if array.size == 1 
+    array.join
+  elsif array.size == 2 
+    array.join(" and ")
+  else 
+    save = array[-1]
+    array.pop
+    string = array.join(", ")
+    string << ", and #{save}"
+  end 
 end
